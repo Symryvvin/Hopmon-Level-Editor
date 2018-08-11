83a817d356ec4c2812eb75459020a33cbea0b026
Aizen <symryvvin@gmail.com>
2018-08-11 18:54:23 +0300

    Add main menu and MenuController

---
+++ src/main/java/ru/aizen/app/controller/MenuController.java
@@ -0,0 +1,42 @@
+package ru.aizen.app.controller;
+
+import javafx.fxml.FXML;
+
+public class MenuController {
+    
+    @FXML
+    private void onClickNew() {
+    }
+
+    @FXML
+    private void onClickOpen() {
+    }
+
+    @FXML
+    private void onClickSave() {
+    }
+
+    @FXML
+    private void onClickSaveAs() {
+    }
+
+    @FXML
+    private void onClickImportHml() {
+    }
+
+    @FXML
+    private void onClickImportHmp() {
+    }
+
+    @FXML
+    private void onClickExportHml() {
+    }
+
+    @FXML
+    private void onClickExportHmp() {
+    }
+
+    @FXML
+    private void onClickQuit() {
+    }
+}
